a453401c64f7756f2bd282bf74b80f8fa3697db5
Marcin <the-thing@users.noreply.github.com>
2026-06-15 09:39:56 +0200

    effect and extended effect enum

---
+++ chip-groove-player/src/main/java/com/github/thething/chipgroove/mod/Effect.java
@@ -0,0 +1,45 @@
+package com.github.thething.chipgroove.mod;
+
+public enum Effect {
+
+    ARPEGGIO(0x00),
+    SLIDE_UP(0x01),
+    SLIDE_DOWN(0x02),
+    TONE_PORTAMENTO(0x03),
+    VIBRATO(0x04),
+    TONE_PORTAMENTO_WITH_VOLUME_SLIDE(0x05),
+    VIBRATO_WITH_VOLUME_SLIDE(0x06),
+    TREMOLO(0x07),
+    SET_PANNING_POSITION(0x08),
+    SET_SAMPLE_OFFSET(0x09),
+    VOLUME_SLIDE(0x0A),
+    POSITION_JUMP(0x0B),
+    SET_VOLUME(0x0C),
+    PATTERN_BREAK(0x0D),
+    EXTENDED_EFFECT(0x0E),
+    SET_SPEED(0x0F);
+
+    private static final Effect[] EFFECT_BY_CODE;
+
+    static {
+        EFFECT_BY_CODE = new Effect[values().length];
+
+        for (int i = 0; i < values().length; i++) {
+            EFFECT_BY_CODE[i] = values()[i];
+        }
+    }
+
+    private final int code;
+
+    Effect(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static Effect valueOf(int code) {
+        return EFFECT_BY_CODE[code];
+    }
+}
---
+++ chip-groove-player/src/main/java/com/github/thething/chipgroove/mod/ExtendedEffect.java
@@ -0,0 +1,45 @@
+package com.github.thething.chipgroove.mod;
+
+public enum ExtendedEffect {
+
+    SET_FILTER(0x00),
+    FINESLIDE_UP(0x01),
+    FINESLIDE_DOWN(0x02),
+    SET_GLISSANDO(0x03),
+    SET_VIBRATO_WAVEFORM(0x04),
+    SET_FINETUNE_VALUE(0x05),
+    LOOP_PATTERN(0x06),
+    SET_TREMOLO_WAVEFORM(0x07),
+    ROUGH_PANNING(0x08),
+    RETRIGGER_SAMPLE(0x09),
+    FINE_VOLUME_SLIDE_UP(0x0A),
+    FINE_VOLUME_SLIDE_DOWN(0x0B),
+    CUT_SAMPLE(0x0C),
+    DELAY_SAMPLE(0x0D),
+    DELAY_PATTERN(0x0E),
+    INVERT_LOOP(0x0F);
+
+    private static final ExtendedEffect[] EFFECT_BY_CODE;
+
+    static {
+        EFFECT_BY_CODE = new ExtendedEffect[values().length];
+
+        for (int i = 0; i < values().length; i++) {
+            EFFECT_BY_CODE[i] = values()[i];
+        }
+    }
+
+    private final int code;
+
+    ExtendedEffect(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static ExtendedEffect valueOf(int code) {
+        return EFFECT_BY_CODE[code];
+    }
+}
