11d9ee5a03bce65d1cb44879d9069a4c66ed9318
Marcin <the-thing@users.noreply.github.com>
2026-07-08 17:40:04 +0200

    test stubs

--- chipamp-player/src/test/java/com/github/thething/chipamp/common/RequirementsTest.java
+++ chipamp-player/src/test/java/com/github/thething/chipamp/common/RequirementsTest.java
@@ -1,8 +1,58 @@
 package com.github.thething.chipamp.common;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RequirementsTest {
 
-    // TODO
+    @Test
+    void shouldReturnValueWhenInRange() {
+        assertThat(Requirements.requireInRange(10, 0, 20)).isEqualTo(10);
+        assertThat(Requirements.requireInRange(0, 0, 20)).isEqualTo(0);
+        assertThat(Requirements.requireInRange(20, 0, 20)).isEqualTo(20);
+        assertThat(Requirements.requireInRange(30, 30, 30)).isEqualTo(30);
+
+        assertThat(Requirements.requireInRange(10.0f, 0.0f, 20.0f)).isEqualTo(10.0f);
+        assertThat(Requirements.requireInRange(0.0f, 0.0f, 20.0f)).isEqualTo(0.0f);
+        assertThat(Requirements.requireInRange(20.0f, 0.0f, 20.0f)).isEqualTo(20.0f);
+        assertThat(Requirements.requireInRange(30.0f, 30.0f, 30.0f)).isEqualTo(30.0f);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOutOfRange() {
+        assertThatThrownBy(() -> Requirements.requireInRange(-10, 0, 10))
+                .hasMessage("Value must be in range [0,10]: -10")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+
+        assertThatThrownBy(() -> Requirements.requireInRange(20, 0, 10))
+                .hasMessage("Value must be in range [0,10]: 20")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+
+        assertThatThrownBy(() -> Requirements.requireInRange(-10.0f, 0.0f, 10.0f))
+                .hasMessage("Value must be in range [0.0,10.0]: -10.0")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+
+        assertThatThrownBy(() -> Requirements.requireInRange(20.0f, 0.0f, 10.0f))
+                .hasMessage("Value must be in range [0.0,10.0]: 20.0")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRangeIsInvalid() {
+        assertThatThrownBy(() -> Requirements.requireInRange(10, 20, 10))
+                .hasMessage("Invalid range [20,10]")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+
+        assertThatThrownBy(() -> Requirements.requireInRange(10.0f, 20.0f, 10.0f))
+                .hasMessage("Invalid range [20.0,10.0]")
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasNoCause();
+    }
 }
